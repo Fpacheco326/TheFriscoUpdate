28148c9a4e9f7b9ac4340344fef6ee3f5339ac20
Fernando Pacheco <fpacheco326@gmail.com>
2019-05-24 03:21:46 -0700

    updates

--- friscoScraper.js
+++ friscoScraper.js
@@ -5,8 +5,17 @@ var mongojs = require("mongojs");
 var app = express();
 var Nightmare = require("nightmare");
 var bodyParser = require('body-parser');
+var exhbs = require("express-handlebars");
 
+// var PORT = process.env.PORT || 3000;
 
+// var routes = require("./routes");
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+app.engine("handlebars", exhbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 console.log("\n***********************************\n" +
     "Grabbing every thread name and link\n" +
     "from SFExaminer:" +
@@ -16,8 +25,10 @@ console.log("\n***********************************\n" +
 var databaseUrl = process.env.MONGODB_URI || "scraper_db";
 var collections = ["scrapedData"];
 
-app.use(bodyParser());
 var db = mongojs(databaseUrl, collections);
+
+// app.use(bodyParser());
+
 
 db.on("error", function(error) {
     console.log("Database Error:", error);
@@ -42,99 +53,100 @@ app.post("/scrapedData", function(req, res){
     });
 });
 
-function scrape(url){
-    console.log(url);
-    var scrape = new Nightmare({
-        show: true
-    })
-    .goto(url)
-    .evaluate(function(){
-        return document.body.innerHTML;
-    }).end().then(function(html){
-        if (html == undefined) {
-            console.log('==================');
-            console.log('html not there!');
-            console.log(arg);
-            console.log('==================');
-            return;
-        }
+// function scrape(url){
+//     console.log(url);
+//     var scrape = new Nightmare({
+//         show: true
+//     })
+//     .goto(url)
+//     .evaluate(function(){
+//         return document.body.innerHTML;
+//     }).end().then(function(html){
+//         if (html == undefined) {
+//             console.log('==================');
+//             console.log('html not there!');
+//             console.log(arg);
+//             console.log('==================');
+//             return;
+//         }
 
-        var $ = cheerio.load(html);
-    })
-};
+//         var $ = cheerio.load(html);
+//     })
+// };
 
-var url = 'https://www.sfexaminer.com/';
+// var url = 'https://www.sfexaminer.com/';
 
-function friscoScraper(){
-	var doIt = new Nightmare({
-	          show: true
-          }).goto(url).type('input[title="Search"]', 'github nightmare').click('.searchsubmit')
-          .wait('#links .result__a')
-      .evaluate(() => document.querySelector('#links .result__a').href)
-    //   .end()
-      .then(console.log)
-  .catch(error => {
-    console.error('Search failed:', error)
-  });
-}
+// function friscoScraper(){
+// 	var doIt = new Nightmare({
+// 	          show: true
+//           }).goto(url).type('input[title="Search"]', 'github nightmare').click('.searchsubmit')
+//           .wait('#links .result__a')
+//       .evaluate(() => document.querySelector('#links .result__a').href)
+//     //   .end()
+//       .then(console.log)
+//   .catch(error => {
+//     console.error('Search failed:', error)
+//   });
+// }
 
-friscoScraper();
+// friscoScraper();
+
+
+var scrape = function(){
+
+return axios.get("https://www.sfgate.com/bayarea/").then(function (response,res) {
+
+
+    var $ = cheerio.load(response.data);
+
+
+    var results = [];
+
+
+    $("p.title").each(function (i, element) {
+
+
+        var title = $(element).text();
+
+        var link = $(element).children().attr("href");
+
+
+        results.push({
+            title: title,
+            link: link
+        });
+        res.send("Scrape Complete");
+    });
 scrape(url);
-
-
-
-// axios.get("https://www.sfgate.com/bayarea/").then(function (response,res) {
-
-
-//     var $ = cheerio.load(response.data);
-
-
-//     var results = [];
-
-
-//     $("p.title").each(function (i, element) {
-
-
-//         var title = $(element).text();
-
-//         var link = $(element).children().attr("href");
-
-
-//         results.push({
-//             title: title,
-//             link: link
-//         });
-//         res.send("Scrape Complete");
-//     });
-//     db.on("error", function (error) {
-//         console.log("Database Error:", error);
-//     });
+    db.on("error", function (error) {
+        console.log("Database Error:", error);
+    });
     
     
-//     app.get("/", function (req, res) {
-//         res.send(cheerio);
-//     });
+    app.get("/", function (req, res) {
+        res.send(cheerio);
+    });
     
-//     app.get("/all", function (req, res) {
+    app.get("/all", function (req, res) {
      
-//         db.scrapedData.find({}, function (error, found) {
+        db.scrapedData.find({}, function (error, found) {
     
-//             if (error) {
-//                 console.log(error);
-//             }
+            if (error) {
+                console.log(error);
+            }
     
-//             else {
-//                 res.json(found);
-//             }
-//         });
-//     });
+            else {
+                res.json(found);
+            }
+        });
+    });
     
 
 
-//     console.log(results);
-    
-// });
-
+    console.log(results);
+   
+});
+}
 
 app.listen(3000, function () {
     console.log("App running on port 3000!");
